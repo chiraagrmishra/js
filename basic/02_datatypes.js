d0d9016ec3cc8d40e996cdc2327349191294160b
chiraagrmishra <161428092+chiraagrmishra@users.noreply.github.com>
2024-02-27 09:06:11 +0000

    config

---
+++ basic/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";// treat all js code as new version
+//alert( 3+3) using node js and not browser
+
+console.log(3+3) ;
+console.log("chiraag")//code readability can use extensions
+
+let name="chiraag"
+let age=19
+let isloggedin=false
+
+//number,bigint,string,boolean,null=stand alone value,undefined,symbol,object
+
+console.log(typeof undefined)
